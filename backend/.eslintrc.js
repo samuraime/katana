15cb76989333b7d00f347e3caf77e024ce09c856
BinkiZhao <samurai7@foxmail.com>
2019-09-09 23:42:43 +0800

    chore: eslint typescript on commit

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   env: {
     commonjs: true,
     es6: true,
     node: true,
     jest: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'prettier', 'plugin:@typescript-eslint/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
+    sourceType: 'module',
   },
   rules: {},
 };
