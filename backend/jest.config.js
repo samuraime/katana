512dfb90551ccd5af0985e32034e718ff3557f43
Minhui.Zhao <minhui.zhao@thoughtworks.com>
2019-09-09 20:16:58 +0800

    chore: typescript

---
+++ backend/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+    },
+  },
+  moduleFileExtensions: ['js', 'ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/src/**/*.test.(ts|js)'],
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+};
