6ba46b22d50ea8b014f5cd389fc98e294068fae2
Minhui.Zhao <minhui.zhao@thoughtworks.com>
2019-10-10 18:11:28 +0800

    fix: backend test

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -5,7 +5,12 @@ export default {
   db: process.env.DB,
   port: process.env.PORT,
   publicPath: path.resolve(__dirname, '../../../frontend/build'),
-  frontendEntry: path.resolve(__dirname, '../../../frontend/build/index.html'),
+  frontendEntry: path.resolve(
+    __dirname,
+    process.env.CI
+      ? '../../../frontend/public/index.html'
+      : '../../../frontend/build/index.html'
+  ),
   jwtSecret: process.env.JWT_SECRET,
   home: process.env.SITE_HOME, // https://samuraime.com
   keys: process.env.APP_KEYS ? process.env.APP_KEYS.split(' ') : [],
