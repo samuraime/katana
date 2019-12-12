102dd79d28ddd6ccb0ddd0ff6bb606b4fb3709f4
BinkiZhao <samurai7@foxmail.com>
2019-12-13 00:14:05 +0800

    refactor: start using Surface

--- frontend/src/pages/Home/Timeline.js
+++ frontend/src/pages/Home/Timeline.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Surface from '../../components/Surface';
 import s from './Timeline.module.scss';
 
 function Timeline({ events, ...otherProps }) {
   return (
     <ul {...otherProps}>
       {events.map(event => (
-        <li key={event.start} className={s.event}>
+        <Surface key={event.start} component="li" className={s.event}>
           <span>{event.start}</span>
           <span> ~ </span>
           <span>{event.end}</span>
           <span className={s.title}>{event.title}</span>
-        </li>
+        </Surface>
       ))}
     </ul>
   );
