f47a2c2678819cadb5e132165577330d7842781e
BinkiZhao <samurai7@foxmail.com>
2019-12-16 23:32:44 +0800

    feat: add description to homepage

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Profile from './Profile';
 import Timeline from './Timeline';
 import Surface from '../../components/Surface';
@@ -6,7 +7,6 @@ import samurai from '../../assets/images/samurai.png';
 import s from './Home.module.scss';
 
 const socialMedias = [{ name: 'GitHub', link: 'https://github.com/samuraime' }];
-
 const timelineEvents = [
   { start: '2019-04', end: '       ', title: 'ThoughtWorks' },
   { start: '2017-10', end: '2018-11', title: 'Meitu' },
@@ -15,10 +15,15 @@ const timelineEvents = [
   // { start: '2015-05', end: '2017-12', title: 'Pinuocao' },
   { start: '2010-09', end: '2014-06', title: 'Northwest A&F University' },
 ];
+const description =
+  '一个心心念念回到长安的秦人, 常自夸为赵二狗, 附会作秦王后裔. 初学耕田, 后以码为梦(或以梦为马), 凭一手 JavaScript 空手套白狼.';
 
 function Home() {
   return (
     <main className={s.root}>
+      <Helmet>
+        <meta name="description" content={description} />
+      </Helmet>
       <Profile
         avatar={samurai}
         title="SamuraiMe"
@@ -33,10 +38,7 @@ function Home() {
         }
       />
       <Surface className={s.intro}>
-        <p>
-          一个心心念念回到长安的秦人, 常自夸为赵二狗, 附会作秦王后裔. 初学耕田,
-          后以码为梦(或以梦为马), 凭一手 JavaScript 空手套白狼.
-        </p>
+        <p>{description}</p>
       </Surface>
       <Timeline events={timelineEvents} />
     </main>
