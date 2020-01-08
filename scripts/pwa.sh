1cd6f626351ccee0e0337254b4edc1ff58cee6cd
BinkiZhao <samurai7@foxmail.com>
2020-01-08 21:31:11 +0800

    fix: remove registerNavigationRoute

--- scripts/pwa.sh
+++ scripts/pwa.sh
@@ -2,3 +2,6 @@
 
 perl -i -pe's|cdn.samuraime.com/katana/index.html|samuraime.com/index.html|g' ./frontend/build/*.{js,json}
 perl -i -pe's|cdn.samuraime.com/katana/service-worker.js|samuraime.com/service-worker.js|g' ./frontend/build/*.{js,json}
+
+# remove registerNavigationRoute in service-worker.js
+perl -i -0pe's|workbox.routing.registerNavigationRoute.+||gs' ./frontend/build/service-worker.js
