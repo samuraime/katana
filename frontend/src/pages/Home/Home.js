8d73e3ed337651f13106bdb4092b604b6bf94d82
BinkiZhao <samurai7@foxmail.com>
2020-01-28 13:04:14 +0800

    refactor: extract personal data to a standalone file

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -5,33 +5,25 @@ import Timeline from './Timeline';
 import Container from '../../components/Container';
 import Surface from '../../components/Surface';
 import samurai from '../../assets/images/samurai.png';
+import profile from './personalProfile';
 import s from './Home.module.scss';
-
-const socialMedias = [{ name: 'GitHub', link: 'https://github.com/samuraime' }];
-const timelineEvents = [
-  { start: '2019-04', end: '       ', title: 'ThoughtWorks' },
-  { start: '2017-10', end: '2018-11', title: 'Meitu' },
-  { start: '2017-02', end: '2017-09', title: 'Weipaitang' },
-  // { start: '2015-12', end: '2017-02', title: 'Chenju' },
-  // { start: '2015-05', end: '2017-12', title: 'Pinuocao' },
-  { start: '2010-09', end: '2014-06', title: 'Northwest A&F University' },
-];
-const description =
-  '一个心心念念回到长安的秦人, 常自夸为赵二狗, 附会作秦王后裔. 初学耕田, 后以码为梦(或以梦为马), 凭一手 JavaScript 空手套白狼.';
 
 function Home() {
   return (
     <Container className={s.root}>
       <Helmet>
-        <meta name="description" content={`SamuraiMe, ${description}`} />
+        <meta
+          name="description"
+          content={`SamuraiMe, ${profile.introduction}`}
+        />
       </Helmet>
       <Profile
         avatar={samurai}
         title="SamuraiMe"
         subheader={
-          <ul className={s.socialMedias}>
-            {socialMedias.map(({ name, link }) => (
-              <li key={name}>
+          <ul className={s.socialMedia}>
+            {profile.socialMedia.map(({ name, link }) => (
+              <li key={name} className={s.socialMediaItem}>
                 <a href={link}>{name}</a>
               </li>
             ))}
@@ -39,9 +31,9 @@ function Home() {
         }
       />
       <Surface className={s.intro}>
-        <p>{description}</p>
+        <p>{profile.introduction}</p>
       </Surface>
-      <Timeline events={timelineEvents} />
+      <Timeline events={profile.timelines} />
     </Container>
   );
 }
---
+++ frontend/src/pages/Home/personalProfile.js
@@ -0,0 +1,17 @@
+export default {
+  introduction:
+    '一个心心念念回到长安的秦人, 常自夸为赵二狗, 附会作秦王后裔. 初学耕田, 后以码为梦(或以梦为马), 凭一手 JavaScript 空手套白狼.',
+  socialMedia: [
+    { name: 'GitHub', link: 'https://github.com/samuraime' },
+    { name: 'Blog', link: 'https://blog.samuraime.com' },
+    // { name: 'Twitter', link: 'https://twitter.com/binkizhao' },
+  ],
+  timelines: [
+    { start: '2019-04', end: '       ', title: 'ThoughtWorks' },
+    { start: '2017-10', end: '2018-11', title: 'Meitu' },
+    { start: '2017-02', end: '2017-09', title: 'Weipaitang' },
+    // { start: '2015-12', end: '2017-02', title: 'Chenju' },
+    // { start: '2015-05', end: '2017-12', title: 'Pinuocao' },
+    { start: '2010-09', end: '2014-06', title: 'Northwest A&F University' },
+  ],
+};
