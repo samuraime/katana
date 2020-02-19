591480524cbe3a6ae6ce56687a42d0d1208fcc5d
Minhui.Zhao <minhui.zhao@thoughtworks.com>
2020-02-19 10:43:12 +0800

    test: await download

--- frontend/src/utils/download.test.js
+++ frontend/src/utils/download.test.js
@@ -1,10 +1,13 @@
 import download from './download';
 
 describe('download', () => {
-  it('should not throw error', () => {
+  it('should not throw error', async () => {
+    global.URL.createObjectURL = jest.fn();
+    global.URL.revokeObjectURL = jest.fn();
     const hello = 'data:text/plain;base64,aGVsbG8%3D';
-    expect(() => {
-      download(hello, 'hello.txt');
-    }).not.toThrow();
+
+    await expect(download(hello, 'hello.txt')).resolves.toBe(undefined);
+    expect(URL.createObjectURL).toBeCalled();
+    expect(URL.revokeObjectURL).toBeCalled();
   });
 });
