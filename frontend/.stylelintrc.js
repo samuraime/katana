ab780b977e855f1f1831c3d4bbc7bc3a6a4e6062
SamuraiMe <samurai7@foxmail.com>
2022-02-11 00:31:37 +0800

    chore: stylelint for styled component

--- frontend/.stylelintrc.js
+++ frontend/.stylelintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
-  "extends": [
+  extends: [
     "stylelint-config-standard",
+    "stylelint-config-styled-components",
     "stylelint-config-prettier",
+  ],
+  overrides: [
+    {
+      files: ['**/*.js'],
+      customSyntax: '@stylelint/postcss-css-in-js',
+    },
   ],
 };
