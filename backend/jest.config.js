8c031941234367c593743c70e58d7a6c2b34fe56
SamuraiMe <samurai7@foxmail.com>
2022-02-12 18:55:15 +0800

    chore(deps): upgraded jest to 27.5.1

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.json',
     },
   },
   setupFiles: ['dotenv/config'],
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
   testMatch: ['**/__tests__/**/*.test.ts'],
   testEnvironment: 'node',
   preset: 'ts-jest',
