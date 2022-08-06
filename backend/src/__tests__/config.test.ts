9ea2b23fb4400920fa4796d4a33c48eec1c0b9d5
Minhui Zhao <samurai7@foxmail.com>
2022-08-06 18:24:20 +1000

    tech: bundled and versioned builds
    - Bundled client and server builds
    - Deploy versioned builds

--- backend/src/__tests__/config.test.ts
+++ backend/src/__tests__/config.test.ts
@@ -6,7 +6,8 @@ describe('app config', () => {
     expect(config).toHaveProperty('db');
     expect(config).toHaveProperty('port');
     expect(config).toHaveProperty('publicPath');
-    expect(config).toHaveProperty('frontendEntry');
+    expect(config).toHaveProperty('clientVersion');
+    expect(config).toHaveProperty('clientVersionsPath');
     expect(config).toHaveProperty('jwtSecret');
     expect(config).toHaveProperty('home');
     expect(config).toHaveProperty('keys');
--- backend/src/middlewares/SPA.ts
+++ backend/src/middlewares/SPA.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+import path from 'path';
 import { Middleware } from 'koa';
 import serialize from 'serialize-javascript';
 import { UserDocument } from '../models/User';
@@ -10,12 +11,22 @@ interface PreloadState {
 
 let entryHTML: string;
 
-function getEntryHTML(): string {
+function getEntryHTML(clientVersion: string): string {
   if (entryHTML) {
     return entryHTML;
   }
 
-  return fs.readFileSync(config.frontendEntry, 'utf8');
+  const clientEntry = process.env.CI
+    ? path.resolve(__dirname, '../../../frontend/public/index.html')
+    : // e.g. client/versions/1.0.0/dist/index.html
+      path.resolve(
+        __dirname,
+        `${config.clientVersionsPath}/${clientVersion}/dist/index.html`
+      );
+
+  const versions = `\n<!-- ClientVersion: ${clientVersion} -->`;
+
+  return fs.readFileSync(clientEntry, 'utf8').concat(versions);
 }
 
 function getPreloadStateScript(state: PreloadState): string {
@@ -23,8 +34,11 @@ function getPreloadStateScript(state: PreloadState): string {
   return `window.__PRELOADED_STATE__=${serialized};`;
 }
 
-function getPreloadStateHTML(state: PreloadState): string {
-  const html = getEntryHTML();
+function getPreloadStateHTML(
+  state: PreloadState,
+  clientVersion: string
+): string {
+  const html = getEntryHTML(clientVersion);
   const script = getPreloadStateScript(state);
 
   return html.replace(
@@ -33,7 +47,7 @@ function getPreloadStateHTML(state: PreloadState): string {
   );
 }
 
-const SPA: Middleware = ctx => {
+const SPA: Middleware = (ctx) => {
   const { user } = ctx.session;
   const state: PreloadState = {
     user: user ? { ...user, id: user._id } : null, // eslint-disable-line no-underscore-dangle
@@ -45,7 +59,15 @@ const SPA: Middleware = ctx => {
     return;
   }
 
-  ctx.body = getPreloadStateHTML(state);
+  if (process.env.ci) {
+    ctx.body = '';
+    ctx.type = 'text/html';
+    return;
+  }
+
+  const clientVersion = ctx.query['client-version'] || config.clientVersion;
+
+  ctx.body = getPreloadStateHTML(state, clientVersion);
   ctx.type = 'text/html';
 };
 
